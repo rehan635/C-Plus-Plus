bd6306b6d7001db43964d137a4ab03fbc3e4f146
Rehan Raja 11 <rehanraja4726@gmail.com>
2024-09-06 20:17:07 +0530

    Add files via upload
    Make Even $ Odd Number Using The If-Else Statement In C++

---
+++ if-else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ using namespace std;
+
+   int main() {
+      
+       int num;
+
+       cout<<"Enter Number :" <<endl;
+       cin >> num;
+
+        if(num%2==0)
+        {
+            cout<<"Even Number" <<endl;
+        }
+        else 
+        {
+            cout<<"Odd Number" <<endl;
+        }
+   }
